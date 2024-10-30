bd186d868f95ca2e4ee464627fb13ca974c94a8d
ecrasevv <francesco.ostuni001@studenti.uniparthenope.it>
2024-10-30 20:25:44 +0100

    write the basics

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 
+// https://beej.us/guide/bgnet/html/split/index.html
+
+#include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <stdbool.h>
+
+#define MAX_CLIENTS 100
 
 uint16_t PORT = 9090;
 struct sockaddr_in server_addr;
+
+// TODO -> check utils
+char *ip = "192.168.1.1";
 
 int main (int argv, const char** argc) {
     // Create server socket
@@ -15,11 +24,26 @@ int main (int argv, const char** argc) {
         perror("server socket fail");
         exit(1);
     }
+    printf("server socket created.\n");
 
+    // Config socket
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.in_addr =  
+    server_addr.sin_addr.s_addr = INADDR_ANY;
     
+    if (bind(server_socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("server bind fail");
+        exit(1);
+    }
+    printf("server bind done.\n");
+
+    if (listen(server_socket_fd, MAX_CLIENTS) < 0) {
+        perror("server listen fail.");
+        exit(1);
+    }
+    printf("server listen done\n");
+
+    while(true) {}
 
     return 0;
 }

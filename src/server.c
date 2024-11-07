ce1c743269c944c96e78f3fefac35506c669f37d
ecrasevv <francesco.ostuni001@studenti.uniparthenope.it>
2024-11-07 20:04:56 +0100

    fix: better fgets rval handling

--- src/server.c
+++ src/server.c
@@ -34,7 +34,7 @@ struct sockaddr_in server_address, client_address;
 int main (int argv, const char** argc) {
     // utils for client connection
     int         client_socket_fd;
-    char        buffer[BUFFER_SIZE] = {0};
+    char        buffer[BUFFER_SIZE];
     ssize_t     bytes_read;
     socklen_t   client_addrlen = sizeof(client_address);
 
@@ -44,7 +44,7 @@ int main (int argv, const char** argc) {
     int         number_of_connections = 0;
 
     if ((server_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("server socket failed.");
+        perror("server socket failed");
         exit(1);
     }
 
@@ -54,51 +54,52 @@ int main (int argv, const char** argc) {
 
     // binding
     if ((bind(server_socket_fd, (struct sockaddr*)&server_address, sizeof(server_address))) == -1) {
-        perror("server binding failed.");
+        perror("server binding failed");
         exit(1);
     }
  
     // listening 
     if ((listen(server_socket_fd, MAX_CLIENTS) == -1)) {
-        perror("server listen failed.");
+        perror("server listen failed");
         exit(1);
     }
-    printf("Server listening on port %d\n", PORT);
+    printf("[+] server listening on port %d\n", PORT);
 
     // handle connections
     for (int i = 0; i < MAX_CLIENTS; ++i ) {
         if ((client_socket_fd = accept(server_socket_fd, (struct sockaddr*)&client_address, &client_addrlen)) == -1) {
-            perror("server accept client connection failed.");
+            perror("server accept client connection failed");
             exit(1);
         }
 
-        printf("server recived a connection!\n");
+        printf("[+] server recived a connection!\n");
 
         number_of_connections++;
 
         if ((bytes_read = read(client_socket_fd, buffer, sizeof(buffer))) == -1) {
-            perror("server read error.");
+            perror("server read error");
             exit(1);
         }
 
-        printf("SERVER READ: %s [%zd]\n", buffer, bytes_read);
+        buffer[strcspn(buffer, "\n")] = 0;
+        printf("[<-] server read: %s [%zd]\n", buffer, bytes_read);
 
         if (Parse_Client_Message(buffer)){
-            printf("Client said the BANNED_WORD!\n");
+            printf("[!] client said the BANNED_WORD!\n");
             if (send(client_socket_fd, WARNING_SERVER_MESSAGE, strlen(WARNING_SERVER_MESSAGE), 0) == -1) {
-                perror("server send error.");
+                perror("server send error");
                 exit(1);
             }
             close(client_socket_fd);
         } else {
             if (send(client_socket_fd, server_message, strlen(server_message), 0) == -1) {
-                perror("server send error.");
+                perror("server send error");
                 exit(1);
             }
-            printf("server sended the message. [%s]\n", server_message);
+            printf("[->] server sended the message. [%s]\n", server_message);
         }
     }
-    printf("total connections handled by server: %d\n", number_of_connections);
+    printf("[+] total connections handled by server: %d\n", number_of_connections);
 
     close(server_socket_fd);
     close(client_socket_fd);
